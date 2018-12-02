e2b30131094b9d45e76eecd65c68dbcd829418fb
florianfreier <flospielt@posteo.de>
2018-12-02 23:28:40 +0100

    initial commit

---
+++ app/app.compnent.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+	
+@Component({
+	selector: 'mw-app',
+	templateUrl: 'app/app.component.html',
+	styles: [`
+		h1 { color: #ffffff; }
+		.description {
+			font-style: italic;
+			color: green;
+		}
+	`]
+})
+export class AppComponent {
+
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,41 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
-import { CategoryListPipe } from './category-list.pipe';
-import { MediaItemFormComponent } from './media-item-form.component';
-import { MediaItemService } from './media-item.service';
-import { lookupListToken, lookupLists } from './providers';
-import { MockXHRBackend } from './mock-xhr-backend';
-import { routing } from './app.routing';
+import { AppComponent } from './app.compnent';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    routing
-  ],
-  declarations: [
-    AppComponent,
-    MediaItemComponent,
-    MediaItemListComponent,
-    FavoriteDirective,
-    CategoryListPipe,
-    MediaItemFormComponent
-  ],
-  providers: [
-    MediaItemService,
-    { provide: lookupListToken, useValue: lookupLists },
-    { provide: HttpXhrBackend, useClass: MockXHRBackend }
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+	imports: [
+		BrowserModule
+		],
+	declarations: [ 
+		AppComponent
+		],
+	bootstrap: [
+		AppComponent
+	]
 })
-export class AppModule {}
+
+export class AppModule{
+	//console.log("Fuck me");
+	}
